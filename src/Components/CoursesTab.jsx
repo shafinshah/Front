64dae5d90892c6372ca92ca2c940d668ec760f46
shafinshah <shafinshah22@gmail.com>
2025-12-22 12:19:58 +0530

    Update CoursesTab.jsx
    backend url change

--- src/Components/CoursesTab.jsx
+++ src/Components/CoursesTab.jsx
@@ -14,7 +14,7 @@ const[query, setQuery] = useState('');
 
 useEffect(()=>{
 
-   axios.get('https://learning2-5nk4.onrender.com/Courses')
+   axios.get('https://e-learning-backend-db0v.onrender.com/Courses')
 .then(courses =>
   setCourses(courses.data))
   .catch(err =>
